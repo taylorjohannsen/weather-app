012aa99eeaad366fb16925dd243a9a1a334a78f9
Taylor Johannsen <freakshow_tj@yahoo.com>
2019-01-04 16:57:44 -0800

    Updates

---
+++ precache-manifest.b694d39e5e9e4db7a6b7ec34f603e051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "dfcf455d8e99dfceac91",
+    "url": "/weather-app/static/js/main.dfcf455d.chunk.js"
+  },
+  {
+    "revision": "978e26d81b822e4f564c",
+    "url": "/weather-app/static/js/1.978e26d8.chunk.js"
+  },
+  {
+    "revision": "dfcf455d8e99dfceac91",
+    "url": "/weather-app/static/css/main.4d2fad70.chunk.css"
+  },
+  {
+    "revision": "e52354211febaa65913bf3751e5c939b",
+    "url": "/weather-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/weather-app/precache-manifest.b694d39e5e9e4db7a6b7ec34f603e051.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/weather-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
